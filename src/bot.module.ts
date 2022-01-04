b5cdb9dc259853efee2f0f1fa3e47cc8f2ccf84d
Mnigos <makowskiigor@gmail.com>
2022-01-04 21:38:38 +0100

    chore(bot-module): add ConfigModule

--- src/bot.module.ts
+++ src/bot.module.ts
@@ -1,9 +1,11 @@
 import { DiscordModule } from '@discord-nestjs/core'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { Intents } from 'discord.js'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     DiscordModule.forRoot({
       token: process.env.DISCORD_TOKEN,
       commands: ['**.*.command.ts'],
